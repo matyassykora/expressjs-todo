7d49d83f45fe01bcff3ee036da965c8de2f3d7af
matyasssykora <96580182+matyassykora@users.noreply.github.com>
2023-12-18 14:08:12 +0100

    Rename assets dir to public

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   const { filter } = req.query;
